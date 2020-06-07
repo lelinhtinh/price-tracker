2d023b9eb8342f0f34543589ac01143893fd3b08
lelinhtinh <lelinhtinh2013@gmail.com>
2020-06-08 00:42:14 +0700

    Format code

--- src/js/background.js
+++ src/js/background.js
@@ -3,9 +3,9 @@ import * as utils from './utils.js';
 let matchUrls = ['https://github.com/lelinhtinh/*'];
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-    (info) => {
+    info => {
         const headers = info.requestHeaders;
-        headers.forEach((header) => {
+        headers.forEach(header => {
             if (header.name.toLowerCase() == 'user-agent') {
                 header.value =
                     'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36';
@@ -29,13 +29,13 @@ chrome.browserAction.onClicked.addListener(() => {
     });
 });
 
-chrome.runtime.onMessage.addListener(async (message) => {
+chrome.runtime.onMessage.addListener(async message => {
     if (message.action !== 'tracking-selector') return;
     await utils.setItem(message.data);
     utils.notify('ok', message.data.title, message.data.url);
 });
 
-chrome.storage.onChanged.addListener((changes) => {
+chrome.storage.onChanged.addListener(changes => {
     const newValue = changes.trackingList ? changes.trackingList.newValue || [] : [];
     if (newValue.length) updateTask(newValue);
 });
@@ -45,15 +45,15 @@ chrome.storage.onChanged.addListener((changes) => {
  */
 async function updateTask(currList) {
     let allTasks = await utils.getAllTasks();
-    allTasks.forEach((task) => {
-        if (currList.findIndex((item) => task.name === item.url) !== -1) utils.removeTask(task);
+    allTasks.forEach(task => {
+        if (currList.findIndex(item => task.name === item.url) !== -1) utils.removeTask(task);
     });
     currList.forEach(utils.createTask);
 }
 
 chrome.alarms.onAlarm.addListener(utils.fireTask);
 
-(async function () {
+(async function() {
     let allTasks = await utils.getAllTasks();
     let allList = await utils.getAllList();
 
@@ -62,6 +62,5 @@ chrome.alarms.onAlarm.addListener(utils.fireTask);
         await utils.cleanTask();
     }
 
-    console.log(allTasks, allList);
     allList.forEach(utils.createTask);
 })();
